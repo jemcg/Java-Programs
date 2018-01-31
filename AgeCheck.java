c70083747614512257bb80d049b415226aa369f8
JaimeM <mcgovernjaime1@gmail.com>
2018-01-31 05:22:02 +0000

    Small update

--- AgeCheck.java
+++ AgeCheck.java
@@ -5,16 +5,15 @@ public class AgeCheck {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
+
         System.out.println("How old are you?");
         int age = Integer.parseInt(reader.nextLine());
-        
+
         if (age >= 0 && age <= 120) {
-        System.out.println("OK");
-    } else if (age < 0 | age > 120) {
+            System.out.println("OK");
+        } else if (age < 0 | age > 120) {
             System.out.println("Impossible!");
-    }
-                
+        }
 
     }
 }
